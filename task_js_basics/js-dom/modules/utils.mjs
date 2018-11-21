3d492de34135bd027a3f8694d6e5cff9473410e6
Oleh <shtepa.oleh.m@gmail.com>
2018-11-21 10:57:44 +0200

    Fix generation menu

--- task_js_basics/js-dom/modules/utils.mjs
+++ task_js_basics/js-dom/modules/utils.mjs
@@ -1,12 +1,30 @@
 import data from './data.mjs';
 
+const generateMenu = (data) => {
+    if (data.length > 0) {
+        if (data.every(menuItem => { return menuItem.title && menuItem.link && menuItem.content && menuItem.icon })) {
+            let menu = document.createElement("ul");
+            data.forEach(el => {
+                let li = document.createElement("li");
+                li.className = "menu_item";
+                let a = document.createElement("a");
+                a.href = el.link;
+                let div = document.createElement("div");
+                div.className = "left_border";
+                a.appendChild(div);
+                a.innerHTML += `${el.title}${el.icon}`;
+                li.appendChild(a);
+                menu.appendChild(li);
+            })
+            return menu;
+        }
+    }
+    return false;
+}
+
 const init = () => {
-    let navigation = document.querySelector(".navigation");
-    let menu = document.createElement("ul");
-    data.forEach(el => {
-        menu.insertAdjacentHTML("beforeend", `<li class='menu_item'><a href="${el.link}"><div class="left_border"></div>${el.title}${el.icon}</a></li>`);
-    })
-    navigation.appendChild(menu);
+    let menu = generateMenu(data);
+    menu ? document.querySelector(".navigation").appendChild(menu) : alert("Error menu generation");
 }
 
 export default {
