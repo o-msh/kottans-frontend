756f8ed303593440e4339db82405b617800a8a1a
Oleh <shtepa.oleh.m@gmail.com>
2018-11-21 09:36:00 +0200

    Add active style for menu elements

--- task_js_basics/js-dom/modules/utils.mjs
+++ task_js_basics/js-dom/modules/utils.mjs
@@ -4,7 +4,7 @@ const init = () => {
     let navigation = document.querySelector(".navigation");
     let menu = document.createElement("ul");
     data.forEach(el => {
-        menu.insertAdjacentHTML("beforeend", `<li class='menu_item'><a href="${el.link}">${el.title}${el.icon}</a></li>`);
+        menu.insertAdjacentHTML("beforeend", `<li class='menu_item'><a href="${el.link}"><div class="left_border"></div>${el.title}${el.icon}</a></li>`);
     })
     navigation.appendChild(menu);
 }
